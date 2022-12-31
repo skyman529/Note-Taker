6a73ecfea6b94ee888a1f78041e511dd15644966
skyman529 <skyman529@gmail.com>
2022-12-31 11:09:23 -0700

    updated server.js

---
+++ Develop/public/assets/js/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const PORT = process.env.PORT || 3001;
+
+// get the ntes
+app.get("/api/notes",(req,res)=>{
+    fs.readFile(__dirname + "/" + "./db.json", "utf8", (err, data) => 
+    {
+        let notes = JSON.parse(data);
+        res.send(notes);
+        console.log("cannot find notes");
+    })
+});
+
+let noteTwo = {
+    title: "test two",
+    note: "test two"
+}
+
+let data = JSON.stringify(noteTwo);
+
+fs.writeFile("./db.json", data, (err) => {
+    if(err) throw (err);
+    console.log("data is written to file")
+})
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
