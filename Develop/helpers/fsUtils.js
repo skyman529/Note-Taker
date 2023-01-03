afb3fb64145a911d9016d19a274385338bbc2c97
skyman529 <skyman529@gmail.com>
2023-01-03 15:04:22 -0700

    Update fsUtils.js

--- Develop/helpers/fsUtils.js
+++ Develop/helpers/fsUtils.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const util = require('util');
 
-// Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
 /**
  *  Function to write data to the JSON file given a destination and some content
